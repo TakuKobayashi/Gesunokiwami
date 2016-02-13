d7a56dd6291769086d42e4cd082d144467c82577
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2016-02-13 21:06:56 +0900

    握り具合を定義

--- app/src/main/java/day/hack/com/gesunokiwami/Config.java
+++ app/src/main/java/day/hack/com/gesunokiwami/Config.java
@@ -4,4 +4,11 @@ public class Config {
 	public static final String TAG = "gesu";
 	public static final String ROOT_URL = "http://battle7.elasticbeanstalk.com/";
 	public static final String SOCKET_SERVER_ROOT_URL = "http://10.10.60.1:8081/";
+
+	public enum GrabState{
+		touch,
+		suprise,
+		pervert,
+		monster
+	}
 }
